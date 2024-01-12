945691b3e9f36e0c4a7df2c5fb281aa39441f1db
Max <41009925+MAKblC@users.noreply.github.com>
2024-01-12 12:38:04 +0300

    Update 1_1_LED_RGB.ino

--- 1_1_LED_RGB.ino
+++ 1_1_LED_RGB.ino
@@ -1,15 +1,15 @@
-#include <FastLED.h> // библиотеки для FastLED
+#include <FastLED.h> 
 #include <FastLED_GFX.h>
 #include <FastLEDMatrix.h>
-#define NUM_LEDS 8 // количество светодиодов в матрице  
-CRGB leds[NUM_LEDS]; // определяем матрицу (FastLED библиотека)
-#define LED_PIN 18 // пин к которому подключена матрица 
-#define COLOR_ORDER GRB // порядок цветов матрицы 
-#define CHIPSET WS2812 // тип светодиодов    
+#define NUM_LEDS 8  
+CRGB leds[NUM_LEDS]; 
+#define LED_PIN 18 
+#define COLOR_ORDER GRB 
+#define CHIPSET WS2812   
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS); // конфигурация матрицы // matrix configuration
-  FastLED.setBrightness(90); // яркость (0-255), не рекомендуется выставлять много, особенно при питании через USB
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS); 
+  FastLED.setBrightness(90); 
 }
 
 void loop() {
