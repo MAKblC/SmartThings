cc3021fe49c0502e37087bd7bf25e20f99397064
Max <41009925+MAKblC@users.noreply.github.com>
2024-02-06 10:14:26 +0300

    Update 4_0_servo.ino

--- 4_0_servo.ino
+++ 4_0_servo.ino
@@ -1,15 +1,15 @@
-#include <ESP32_Servo.h>
-Servo myservo;
+#include <ESP32_Servo.h> // библиотека для сервопривода
+Servo myservo; // создаем объект
 
 void setup() {
-  myservo.attach(19);
+  myservo.attach(19); // пин сервопривода
   Serial.begin(115200);
 }
 
 void loop() {
   // поворот на угол
   myservo.write(170);
-  Serial.println(myservo.read());
+  Serial.println(myservo.read()); // считывание текущей позиции
   delay(1000);
   myservo.write(10);
   Serial.println(myservo.read());
