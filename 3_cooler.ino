16fbf0df794c31b8f937c0f969eefed2043e3e57
Max <41009925+MAKblC@users.noreply.github.com>
2024-02-06 11:21:39 +0300

    Update 3_cooler.ino

--- 3_cooler.ino
+++ 3_cooler.ino
@@ -1,12 +1,12 @@
-#define WIND 17
+#define WIND 17 // пин модуля реле
 
 void setup() {
-  pinMode(WIND, OUTPUT);
+  pinMode(WIND, OUTPUT); // пин на выход
 }
 
 void loop() {
-  digitalWrite(WIND, HIGH);
+  digitalWrite(WIND, HIGH); // вкл вентилятор
   delay(1000);
-  digitalWrite(WIND, LOW);
+  digitalWrite(WIND, LOW); // выкл вентилятор
   delay(1000);
 }
