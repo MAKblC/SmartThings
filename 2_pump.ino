7426c18e0674419404be7bf61c3a1997bfcadd54
Max <41009925+MAKblC@users.noreply.github.com>
2024-02-06 11:06:26 +0300

    Update 2_pump.ino

--- 2_pump.ino
+++ 2_pump.ino
@@ -1,12 +1,12 @@
-#define PUMP 16
+#define PUMP 16 // пин, к которому подключено реле
 
 void setup() {
-  pinMode(PUMP, OUTPUT);
+  pinMode(PUMP, OUTPUT); // пин на выход
 }
 
 void loop() {
-  digitalWrite(PUMP, HIGH);
+  digitalWrite(PUMP, HIGH); // вкл насос
   delay(1000);
-  digitalWrite(PUMP, LOW);
+  digitalWrite(PUMP, LOW); // выкл насос
   delay(1000);
 }
