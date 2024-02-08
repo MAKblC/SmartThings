b65e934c30a2635eb20b6ac65642f54b67272bb1
Max <41009925+MAKblC@users.noreply.github.com>
2024-02-08 11:56:55 +0300

    Update 6_light.ino

--- 6_light.ino
+++ 6_light.ino
@@ -1,13 +1,13 @@
-const int pinPhoto = 35;
-int raw = 0;
+const int pinPhoto = 35; // пин фоторезистора (А7)
+int raw = 0; // переменная для хранения данных
 
 void setup() {
   Serial.begin(115200);
-  pinMode( pinPhoto, INPUT );
+  pinMode(pinPhoto, INPUT); // пин на вход
 }
 
 void loop() {
-  raw = analogRead( pinPhoto );
+  raw = analogRead(pinPhoto); // считываем значение
   Serial.println( raw );
   delay(200);
 }
